9361d4364df88f1cf03d339d2ab6febaec6f860e
nguyenthanhhoang21095 <thanhhoang021095@gmail.com>
2020-04-12 16:52:11 +0700

    mongoDB and mongoose

---
+++ routes/cart.route.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+// controllers
+var controller = require('../controllers/cart.controller');
+
+router.get('/add/:productId', controller.addToCart);
+
+module.exports = router;
